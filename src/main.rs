fa9718e7b4e93755618ea384c696bc007c0b56ca
willpreebs <wpriebe16124@gmail.com>
2022-11-12 15:17:25 -0500

    added Cargo files

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ fn main() {
 
     let args = std::env::args();
 
-    println!("{}", String::from("Welcome to the rust minishell"));
+    println!("Welcome to the rust minishell");
 
     shell::run_shell(args);
 
 }
+
